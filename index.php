ff5e6bbc646e34a141efd107201fdc43ab609ea3
Roman Pichugin <Roman.Pichugin20@gmail.com>
2022-05-01 15:38:18 +0700

    Lab 7 DataBase v.5

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 
         $db_host = "localhost";
         $db_username = "root";
-        $db_password = "";
+        $db_password = "BadWolf";
         $db_database = "Lab7_Uni";
 
         echo "<table class='table w-75 table-bordered'>";
