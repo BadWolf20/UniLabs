a34f1a8743a435f675d4d8b67984220601754eb3
Roman Pichugin <Roman.Pichugin20@gmail.com>
2022-05-01 18:24:35 +0700

    Lab 7 Actions v.21

--- update.php
+++ update.php
@@ -14,12 +14,12 @@
         die("Ошибка при выборе бызы даных");
     }
 
-    $result = mysqli_query($connection, "UPDATE Student s set
-                        s.Name = '$_POST[Name]',
-                        s.Surname = '$_POST[Surname]',
-                        s.MiddleName = '$_POST[MiddleName]',
-                        s.GroupId = '$_POST[GroupId]'
-                        WHERE s.StudentId = $_POST[StudentId]");
+    $result = mysqli_query($connection, "UPDATE Student SET
+                        Name = $_POST[Name],
+                        Surname = $_POST[Surname],
+                        MiddleName = $_POST[MiddleName],
+                        GroupID = $_POST[GroupId]
+                        WHERE StudentID = $_POST[StudentId]");
 
     if (!$result) {
         die("Ошибка запроса к базе данных</br>") . mysqli_error($connection);
