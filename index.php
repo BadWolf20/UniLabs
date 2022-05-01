b267303f8a5fe4e61617e97947ecac68a5877e83
Roman Pichugin <Roman.Pichugin20@gmail.com>
2022-05-01 13:02:52 +0700

    Lab 7 Task 2

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-print_r(include_once ("./data.php"));
+    echo "<table border = '1'>";
+    $data = (include_once("./data.php"));
+
+    foreach ($data as $key => $group) {
+        echo "<tr><td colspan='2'>" . $key . "</td></tr>";
+        foreach ($group as $person) {
+            echo "<tr><td>" . $person["name"] . "</td>";
+            echo "<td>" . $person["mail"] . "</td></tr>";
+        }
+    }
+    echo '</table>';
 ?>
