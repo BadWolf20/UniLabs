fd6c87271eb2aa994655be39a7e567b72c91af72
Roman Pichugin <Roman.Pichugin20@gmail.com>
2022-05-14 15:27:52 +0700

    Lab3

--- app/Facade/ParserService.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Facade;
-use Illuminate\Support\Facades\Facade;
-class ParserService extends Facade{
-    protected static function getFacadeAccessor()
-{
-    return 'Parser';
-}
-}
